3d53bb5ebbbb9dcf4c557305daf0ed07a55a1b3a
Nikola Raicevic <nmarinosraitsevits@ucsd.edu>
2023-11-18 23:25:04 -0800

    My_OMPL_PKG

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # my_ompl
 The repository that is inspired by the ompl library to create a motion planner. 
+
+## Installation
---
+++ src/my_node.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world my_ompl package\n");
+  return 0;
+}
